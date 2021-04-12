ca3eb11943908c9e54f2846a530fc26c279794eb
Tim Fechner <tim.fechner@mailbox.org>
2021-04-12 20:25:10 +0200

    Prepare for lane creation

--- app/src/components/BoardCreate.vue
+++ app/src/components/BoardCreate.vue
@@ -1,18 +1,43 @@
 <template>
   <div>
-    <h1>Create a Board</h1>
-    <input type="text" v-model="title" placeholder="Board Title">
-    <input type="number" v-model="thinkTimeLimit">
-    <input type="number" v-model="discussTimeLimit">
-    <button type="submit" @click="createBoard">Create</button>
+    <h1 class="text-lg">Create a Board</h1>
+
+    <div class="mt-4">
+      <h2>Board settings</h2>
+      <input type="text" v-model="title" placeholder="Board Title">
+      <input type="number" v-model="thinkTimeLimit">
+      <input type="number" v-model="discussTimeLimit">
+    </div>
+
+    <div class="mt-4">
+      <h2>Lanes</h2>
+      <div class="py-4">
+        <draggable :list="lanes" item-key="title" handle=".handle">
+          <template #item="{element}">
+            <div class="bg-white rounded shadow mt-2 p-2 max-w-md flex items-center">
+              <div class="handle cursor-move mr-4"><MenuIcon class="h-auto w-5"></MenuIcon></div>
+              <div><input type="text" v-model="element.title"></div>
+            </div>
+          </template>
+        </draggable>
+      </div>
+    </div>
+
+    <button class="bg-purple-600 hover:bg-purple-700 text-white font-bold rounded px-2 shadow" type="submit" @click="createBoard">Create</button>
   </div>
 </template>
 
 <script>
+import draggable from "vuedraggable";
+import { MenuIcon } from "@heroicons/vue/outline";
 import api from "../api";
 
 export default {
   name: "BoardCreate",
+  components: {
+    draggable,
+    MenuIcon,
+  },
   data: function() {
     return {
       owner: 1,
